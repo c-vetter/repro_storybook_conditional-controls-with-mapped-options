06fe496a7626d51551160a600b0423b277c46946
Christian Vetter <hallo@cv-o.de>
2022-09-07 15:13:57 +0200

    reproduce bug

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,41 +1,271 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Button } from './Button';
+import { Button } from "./Button";
+
+const controlShown = `<abbr className="control-shown" title="Control Shown">⦿</abbr>`
+const controlHidden = `<abbr className="control-hidden" title="Control Hidden">⦾</abbr>`
+const argApplied = `<abbr className="arg-applied" title="Argument Applied">⊜</abbr>`
+const argIgnored = `<abbr className="arg-ignored" title="Argument Ignored">⦶</abbr>`
+const good = `<abbr className="good" title="Control and Arg in sync">✔</abbr>`
+const bad = `<abbr className="bad" title="Control and Arg out of sync">✘</abbr>`
+
+const withControl = {
+  true: controlShown,
+  false: controlHidden,
+}
+const withArg = {
+  true: argApplied,
+  false: argIgnored,
+}
+
+const conditions = {
+  exists: {
+    true: {
+      children: {
+        undefined: { arg: false, control: true },
+        null: { arg: true, control: true },
+        true: { arg: true, control: true },
+        false: { arg: true, control: true },
+        string: { arg: true, control: true },
+        element: { arg: true, control: true },
+      },
+      size: {
+        undefined: { arg: false, control: false },
+        set: { arg: true, control: true },
+      },
+    },
+    false: {
+      children: {
+        undefined: { arg: true, control: false },
+        null: { arg: false, control: false },
+        true: { arg: false, control: false },
+        false: { arg: false, control: false },
+        string: { arg: false, control: false },
+        element: { arg: false, control: false },
+      },
+      size: {
+        undefined: { arg: true, control: true },
+        set: { arg: false, control: false },
+      },
+    },
+  },
+  truthy: {
+    true: {
+      children: {
+        undefined: { arg: false, control: true },
+        null: { arg: false, control: true },
+        true: { arg: true, control: true },
+        false: { arg: false, control: true },
+        string: { arg: true, control: true },
+        element: { arg: true, control: true },
+      },
+      size: {
+        undefined: { arg: false, control: false },
+        set: { arg: true, control: true },
+      },
+    },
+    false: {
+      children: {
+        undefined: { arg: true, control: false },
+        null: { arg: true, control: false },
+        true: { arg: false, control: false },
+        false: { arg: true, control: false },
+        string: { arg: false, control: false },
+        element: { arg: false, control: false },
+      },
+      size: {
+        undefined: { arg: true, control: true },
+        set: { arg: false, control: false },
+      },
+    },
+  },
+  eq: {
+    string: {
+      children: {
+        undefined: { arg: false, control: true },
+        null: { arg: false, control: false },
+        true: { arg: false, control: false },
+        false: { arg: false, control: false },
+        string: { arg: false, control: false },
+        element: { arg: false, control: false },
+      },
+      size: {
+        undefined: { arg: false, control: false },
+        set: { arg: false, control: false },
+      },
+    },
+    value: {
+      children: {
+        undefined: { arg: false, control: true },
+        null: { arg: false, control: true },
+        true: { arg: true, control: true },
+        false: { arg: false, control: true },
+        string: { arg: true, control: true },
+        element: { arg: true, control: true },
+      },
+      size: {
+        undefined: { arg: false, control: false },
+        set: { arg: true, control: true },
+      },
+    },
+  },
+  neq: {
+    string: {
+      children: {
+        undefined: { arg: true, control: false },
+        null: { arg: true, control: true },
+        true: { arg: true, control: true },
+        false: { arg: true, control: true },
+        string: { arg: true, control: true },
+        element: { arg: true, control: true },
+      },
+      size: {
+        undefined: { arg: true, control: true },
+        set: { arg: true, control: true },
+      },
+    },
+    value: {
+      children: {
+        undefined: { arg: false, control: true },
+        null: { arg: false, control: true },
+        true: { arg: true, control: true },
+        false: { arg: false, control: true },
+        string: { arg: true, control: true },
+        element: { arg: true, control: true },
+      },
+      size: {
+        undefined: { arg: false, control: false },
+        set: { arg: true, control: true },
+      },
+    },
+  },
+}
+
+const conditionalValues = {
+  true: true,
+  false: false,
+  value: undefined,
+  string: `undefined`,
+}
+
+const data = Object.entries(conditions)
+.flatMap(
+  ([operator, x]) => Object.entries(x).map(
+    ([value, { children, size }]) => ({
+      operator,
+      value,
+      children,
+      size,
+    })
+  )
+)
+.map(({ operator, value, children, size }) => ({
+  operator,
+  value: conditionalValues[value],
+  children,
+  size,
+  argTypeLabel: `${
+    operator[0].toUpperCase()
+  }${
+    operator.slice(1)
+  }${
+    value[0].toUpperCase()
+  }${
+    value.slice(1)
+  }`,
+}))
+
+type D = (typeof data)[number]
+type AC = D[`children`|`size`]
+type X = AC[keyof AC]
+
+function format ({ control, arg }: X) {
+  return `${withControl[control.toString()]}${withArg[arg.toString()]} ${control === arg ? good : bad}`
+}
+
+const options = [
+  `undefined`,
+  `null`,
+  `true`,
+  `false`,
+  `string`,
+  `element`,
+]
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
+  parameters: {
+    controls: { sort: 'alpha' },
+    docs: { description: { component: `
+      | Condition | ${
+        ([
+          ...options.map(v=>[`children`, v] as const),
+          ...[
+            `undefined`,
+            `set`,
+          ].map(v=>[`size`, v] as const),
+        ])
+        .map(([prop, value]) => (`<abbr title="${prop}=${value}">${prop[0]}=${value[0]}</abbr>`))
+        .join(` | `)
+      } |
+      |-|
+      |${data.map(({ operator, value, children, size }) => [
+        `${operator}: ${JSON.stringify(value)}`,
+        ...options.map(o => format(children[o])),
+        format(size.undefined),
+        format(size.set),
+      ].join(` | `)).join(`|\n|`)}|
+    `.replace(/^ +/gm, ``) } }
+  },
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    children: {
+      defaultValue: `undefined`,
+      options,
+      mapping: {
+        undefined: undefined,
+        null: null,
+        true: true,
+        false: false,
+        string: `String`,
+        element: <strong>element</strong>,
+      },
+      control: {
+        type: `radio`,
+      },
+    },
+    ...Object.fromEntries(
+      data
+      .flatMap((entry) => ([
+        {
+          arg: `children`,
+          ...entry,
+        },
+        {
+          arg: `size`,
+          ...entry,
+        },
+      ]))
+      .map(({
+        arg,
+        argTypeLabel,
+        operator,
+        value,
+      }) => ([
+        `${arg}${argTypeLabel}`,
+        {
+          defaultValue: true,
+          if: {
+            arg,
+            [operator]: value,
+          }
+        },
+      ]))
+    )
   },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+export const Repro: ComponentStory<typeof Button> = (args) => <Button {...args} />
--- stories/Button.tsx
+++ stories/Button.tsx
@@ -2,47 +2,25 @@ import React from 'react';
 import './button.css';
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  children: React.ReactChild;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
   size = 'medium',
-  backgroundColor,
-  label,
-  ...props
+  children,
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = 'storybook-button--primary'
+
   return (
     <button
       type="button"
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
     >
-      {label}
+      {children}
     </button>
   );
 };
